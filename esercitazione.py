6339d4ec709d6a807084f8eeda1e439d80bcaf7a
Carni-giantommaso8 Carni-giantommaso8 <carni.giantommaso@iisgalvanimi.edu.it>
2025-02-14 12:02:59 +0000

    punto 1 real

--- esercitazione.py
+++ esercitazione.py
@@ -1,2 +1,20 @@
-print(0)
-print("secondo commit ")
+inventario = {
+0 : {"nome": "Il PC di Giantommaso", "quantità": 1, "prezzo": 3999},
+1 : {"nome": "Nintendo Switch 2", "quantità": 10, "prezzo": 449},
+2 : {"nome": "GTA VI", "quantità": 3, "prezzo": 100}
+}
+contatore = 2
+def aggiunta ():
+    global contatore
+    nome = input("scrivi nome: ")
+    quantità = int(input("scrivi una quantità: "))
+    prezzo = int(input("scrivi prezzo: "))
+    contatore += 1
+    inventario[contatore]= {"nome": nome,"quantità": quantità,"prezzo": prezzo}
+    return  contatore,inventario
+
+print("inserisci 1 per aggiungere uno/a studente")
+while True:
+    comando = int(input("scegli un opzione: "))
+    if comando == 1:
+        print(aggiunta())
